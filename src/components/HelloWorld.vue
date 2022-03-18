c27111b563e630e05882059fed29168d1931ef52
jakub.serwin <j.serwin@digitalcolliers.com>
2022-03-18 11:39:46 +0100

    vite-ts setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
