81477baea30103b4d1332b346bb77818bb9e6d19
jakub.serwin <j.serwin@digitalcolliers.com>
2022-03-21 18:16:59 +0100

    table component

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,27 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 import { useStore } from './store';
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   components: {
-    HelloWorld
+    HelloWorld,
   },
-  setup () {
+  setup() {
     const store = useStore();
     store.getUsers();
 
-    return {}
-  }
-})
+    return {};
+  },
+});
 </script>
 
 <style scoped>
-
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,37 @@
 <template>
-  <div>
-    {{ users }}
+  <div class="wrapper">
+      <n-tabs
+        default-value="table"
+        size="large"
+        justify-content="space-evenly"
+      >
+        <n-tab-pane name="table" tab="Table">
+          <v-table></v-table>
+        </n-tab-pane>
+        <n-tab-pane name="signup" tab="Auth"> card 2 </n-tab-pane>
+      </n-tabs>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
-import { useStore } from '../store';
+import { defineComponent } from 'vue';
+import { NTabs, NTabPane } from 'naive-ui';
+import VTable from './VTable.vue'
 
 export default defineComponent({
+  components: {
+    VTable,
+    NTabs,
+    NTabPane,
+  },
   setup() {
-    const store = useStore();
-  
-    const users = computed(() => store.users);
-
-    return { users };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.wrapper {
+  height: 100vh;
+  padding: 1rem 3rem;
+}
+</style>
