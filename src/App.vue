591c312923d331538594b0c6f3e8c84af2a04f45
jakub.serwin <j.serwin@digitalcolliers.com>
2022-03-23 22:46:33 +0100

    recent changes

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from './store';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  components: {
-    HelloWorld,
-  },
   setup() {
     const store = useStore();
     store.getUsers();
