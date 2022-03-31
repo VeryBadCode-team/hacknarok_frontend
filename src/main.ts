4cf30e5ee820a7f69006b39dc82e306e99a86b5b
WojciechSutkowski <wojciechsutkowskii@gmail.com>
2022-03-31 21:52:18 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
 import router from './router';
-import Toast, { PluginOptions } from 'vue-toastification';
+import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 
 const app = createApp(App);
 
-const options: PluginOptions = {};
-
-app.use(createPinia()).use(router).use(Toast, options);
+app.use(createPinia()).use(router).use(Toast);
 
 app.mount('#app');
