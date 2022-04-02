c23fff4f7054ebed2da391ef30cf34fa90ccc7ab
WojciechSutkowski <wojciechsutkowskii@gmail.com>
2022-04-02 20:09:50 +0200

    added cards and footer

--- src/views/Wrapper/Wrapper.vue
+++ src/views/Wrapper/Wrapper.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <landing-page></landing-page>
+    <cards></cards>
+    <contact></contact>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import LandingPage from './LandingPage/LandingPage.vue';
+import Cards from './Cards/Cards.vue';
+import Contact from './Contact/Contact.vue';
 
 export default defineComponent({
   components: {
     LandingPage,
+    Cards,
+    Contact,
   },
 });
 </script>
