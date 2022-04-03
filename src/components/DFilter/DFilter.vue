81b3d4635d62c24a4ea0f48d7b9954f1f9059f18
jakubserwin <kuba.serwin99@gmail.com>
2022-04-03 04:58:23 +0200

    add dialog

--- src/components/DFilter/DFilter.vue
+++ src/components/DFilter/DFilter.vue
@@ -4,21 +4,32 @@
       <img src="@/assets/images/icons/maginfier.svg" alt="magnifier" />
       <input v-model="filter" class="filter__input" placeholder="Search" />
     </div>
-    <n-button type="primary">Utwórz spotkanie</n-button>
+    <n-button type="primary" @click="toggleModal">Utwórz spotkanie</n-button>
+    <Dialog v-model:visible="isModalOpen" header="Utwórz nowe spotkanie">
+      Content
+    </Dialog>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import { NButton } from 'naive-ui';
+import Dialog from 'primevue/dialog';
 
 export default defineComponent({
   components: {
+    Dialog,
     NButton,
   },
   setup() {
     const filter = ref('');
-    return { filter };
+    const isModalOpen = ref(false);
+
+    const toggleModal = () => {
+      isModalOpen.value = true;
+    };
+
+    return { filter, isModalOpen, toggleModal };
   },
 });
 </script>
